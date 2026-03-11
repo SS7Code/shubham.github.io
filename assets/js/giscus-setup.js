17fb07ba669274de18e7b408ef57bb1c9dd13a37
SS7Code <sharma.shubham.mailbox@gmail.com>
2026-03-11 13:11:04 +0000

    Deploying to gh-pages from @ SS7Code/shubhamsharma.site@810811320f1844f11df7d5a9f6ee7fee29016a36 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SS7Code/shubham.github.io",
+    "data-repo": "SS7Code/shubhamsharma.site",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
